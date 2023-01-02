436b7e1879fbfa2fbf9edae8a4e8fbcced973ce4
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-01-02 14:04:20 -0600

    Dino Upload

--- Assets/Prototypes/Diana/Chaotic_Pull/Code/Grapple_ChaoticPull.cs
+++ Assets/Prototypes/Diana/Chaotic_Pull/Code/Grapple_ChaoticPull.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grapple : MonoBehaviour
+public class Grapple_ChaoticPull : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
