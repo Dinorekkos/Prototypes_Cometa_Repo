8933dad528dfb6004cf49f9ad3143f745c10b754
WilhemB <will_bates@live.com.mx>
2023-01-02 18:50:17 -0600

    Implementacion CharacterController2D

--- Assets/Common/CharacterController2D/Scripts/InputManager.cs
+++ Assets/Common/CharacterController2D/Scripts/InputManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+using UnityEngine.InputSystem;
 
 namespace CometaPrototypes.CharacterController2D
 {
     public class InputManager : MonoBehaviour
     {
-        void Update()
-        {
+        public bool jumpPressed;
+        public Vector2 movementValue;
 
+        public void OnMovementAction(InputAction.CallbackContext context){
+            //movementValue = context.ReadValue<Vector2>();
+            Debug.Log ("OnMovementAction!");
+        }
+        public void OnJumpAction(InputAction.CallbackContext context){
+            //jumpPressed = context.ReadValue<bool>();
+            Debug.Log ("OnJumpAction!");
         }
     }
 }
