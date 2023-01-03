996f6cea762fd4d616b35e00cb8b43c834603175
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-01-03 13:33:52 -0600

    raycast grapple

--- Assets/Prototypes/Diana/Chaotic_Pull/Code/GrappleChaoticPull.cs
+++ Assets/Prototypes/Diana/Chaotic_Pull/Code/GrappleChaoticPull.cs
@@ -1,20 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Diana_ChaoticPull
 { 
     public class GrappleChaoticPull : MonoBehaviour
     {
+        #region variables
+
+
+        Mouse _mouse;
+        private Vector2 _mouseAxis;
+        
+        #endregion
+
+        #region Unity Methods
+
         void Start()
-        {
-            
+        { 
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_EDITOR || UNITY_STANDALONE_LINUX
+            _mouse = Mouse.current;
+#endif
         }
 
         void Update()
         {
-
+            if (_mouse.leftButton.wasPressedThisFrame)
+            {
+                Debug.Log("Left Click");
+                SendRay();       
+            }
         }
+        #endregion
+
+        #region private methods
+
+        void SendRay()
+        {
+            _mouseAxis = _mouse.position.ReadValue();
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(_mouseAxis);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+        #endregion
         
         
     }
