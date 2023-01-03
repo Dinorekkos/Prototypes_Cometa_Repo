e7033f448bcab024773eb56979219b799a38ce3a
WilhemB <will_bates@live.com.mx>
2023-01-03 12:41:26 -0600

    avances implementacion character controller 2d

---
+++ Assets/Common/CharacterController2D/Scripts/CharacterController2D.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CometaPrototypes.CharacterController2D
+{
+    public class CharacterController2D : MonoBehaviour
+    {
+
+    }
+}
--- Assets/Common/CharacterController2D/Scripts/InputManager.cs
+++ Assets/Common/CharacterController2D/Scripts/InputManager.cs
@@ -2,23 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using MoreMountains.Tools;
+using UnityEngine.InputSystem.Controls;
 
 namespace CometaPrototypes.CharacterController2D
 {
     public class InputManager : MonoBehaviour
     {
-        public bool jumpPressed;
-        public Vector2 movementValue;
+        public Input_CharacterController2D InputActions;
 
-        public void OnMovementAction(InputAction.CallbackContext context){
-            //movementValue = context.ReadValue<Vector2>();
-            Debug.Log ("OnMovementAction!");
-        }
-        public void OnJumpAction(InputAction.CallbackContext context){
-            //jumpPressed = context.ReadValue<bool>();
-            Debug.Log ("OnJumpAction!");
+        [SerializeField] private Vector2 _movement;
+        [SerializeField] private bool _jumpPressed;
+
+        public Vector2 Movement { get => _movement; private set => _movement = value; }
+        public bool JumpPressed { get => _jumpPressed; private set => _jumpPressed = value; }
+
+        public MMInput.IMButton JumpButton { get; protected set; }
+
+        private void Awake()
+        {
+            InputActions = new Input_CharacterController2D();
         }
 
-        
+        private void Start()
+        {
+            Initialization();
+        }
+
+        private void Initialization()
+        {
+            InputActions.Player2D.Movement.performed += context => Movement = context.ReadValue<Vector2>();
+            InputActions.Player2D.Jump.performed += context => { OnJumpPerformed(context); };
+        }
+
+        private void OnJumpPerformed(InputAction.CallbackContext context)
+        {
+            var control = context.control;
+
+            if (control is ButtonControl button)
+            {
+                if (button.wasPressedThisFrame)
+                {
+                    JumpPressed = true;
+                }
+                if (button.wasReleasedThisFrame)
+                {
+                    JumpPressed = false;
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            InputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            InputActions.Disable();
+        }
     }
 }
